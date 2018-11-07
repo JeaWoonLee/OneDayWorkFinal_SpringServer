469abe44236dbfbfc590cfbebc98f822c4b25d5d
Edu_1 <Edu_1@LX-EDU1>
2018-11-07 23:00:00 +0900

    일감 신청 구현

--- OneDayWork/src/main/java/com/lx/odw/dao/SeekerServiceImpl.java
+++ OneDayWork/src/main/java/com/lx/odw/dao/SeekerServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.lx.odw.service.SeekerService;
+import com.lx.odw.vo.ProjectCandidateQueueVO;
 import com.lx.odw.vo.ProjectJobListVO;
 import com.lx.odw.vo.ProjectVO;
 
@@ -28,6 +29,46 @@ public class SeekerServiceImpl implements SeekerService{
 	@Override
 	public ProjectVO requestProjectDetail(ProjectVO projectVO) {
 		return seekerDAO.requestProjectDetail(projectVO);
+	}
+
+	@Override
+	public ProjectJobListVO requestJobDetail(ProjectJobListVO vo) {
+		return seekerDAO.requestJobDetail(vo);
+	}
+
+	@Override
+	public int requestTargetDateCount(ProjectCandidateQueueVO vo) {
+		return seekerDAO.requestTargetDateCount(vo);
+	}
+
+	@Override
+	public Integer candidateJob(ProjectCandidateQueueVO vo) {
+		
+		final int ANOTHER_ACCEPTED = 2;
+		final int CANDIDATE_DUPLICATED = 3;
+		final int ACCEPTED_DUPLICATED = 4;
+		//같은 날짜에 수락된 같은 일감이 있는지 확인한다
+		int checkDuplicateAcceptCandidate = seekerDAO.checkDuplicateAcceptCandidate(vo);
+		if(checkDuplicateAcceptCandidate == 1) {
+			return ACCEPTED_DUPLICATED;
+		}
+		
+		//같은 날짜에 같은 일감으로 신청 되어 있는지 확인한다.
+		int checkDuplicateCandidate = seekerDAO.checkDuplicateCandidate(vo);
+		if(checkDuplicateCandidate == 1) {
+			return CANDIDATE_DUPLICATED;
+		}
+		
+		//같은 날짜에 수락된 일감이 있는지 확인한다.
+		int checkAnotherAccepted = seekerDAO.checkAnotherAccepted(vo);
+		if(checkAnotherAccepted == 1) {
+			return ANOTHER_ACCEPTED;
+		}
+		
+		
+		
+		//인서트하기
+		return seekerDAO.candidateJob(vo);
 	}
 
 }
