2797cca482d6871038844a479e0adc04355464cc
KimDongGa <dgkim0419@gmail.com>
2018-11-09 16:16:36 +0900

    KimDongGa- Editing Mapping OfferMobileLogin

--- OneDayWork/src/main/java/com/lx/odw/controller/LoginController.java
+++ OneDayWork/src/main/java/com/lx/odw/controller/LoginController.java
@@ -1,13 +1,10 @@
 package com.lx.odw.controller;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 
 import com.lx.odw.model.LoginModel;
 import com.lx.odw.service.LoginService;
@@ -20,29 +17,13 @@ public class LoginController {
 	@Autowired
 	LoginService service;
 	
-	private final String mainPage = "haruMainPage";
-	
-	
 	@RequestMapping(value="seekerLogin.do", method=RequestMethod.POST)
 	public @ResponseBody SeekerVO seekerLogin(LoginModel model) {
 		return service.seekerLogin(model);
 	}
 	
-
-	@RequestMapping("offerLogin.do")
-	public String offerLogin() {
-		System.out.println("offerLogin이 실행됨");
-		return "offerLogin";
+	@RequestMapping(value="offerMobileLogin.do", method=RequestMethod.POST)
+	public @ResponseBody OfferVO offerLogin(LoginModel model) {
+		return service.offerLogin(model);
 	}
-	
-	@RequestMapping(value="offerLogin.do", method=RequestMethod.POST)
-	public String offerLogin(LoginModel model, HttpSession session) {
-		System.out.println("offerLogin.do 이 실행됨");
-		
-		OfferVO offerVO= service.offerLogin(model);
-		session.setAttribute("offerId",offerVO);
-		
-		return mainPage;
-	}
-
 }
