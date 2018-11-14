8ff6d9f832a4052aa4014e21dfce96eb2b24a767
zaun1290@naver.com <wpdns1290>
2018-11-14 19:52:58 +0900

    회원가입 기능 구현 일감 목록 화면, 기능 구현

--- OneDayWork/src/main/java/com/lx/odw/controller/OfferController.java
+++ OneDayWork/src/main/java/com/lx/odw/controller/OfferController.java
@@ -2,15 +2,19 @@ package com.lx.odw.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.lx.odw.dao.OfferDAO;
 import com.lx.odw.service.OfferService;
-import com.lx.odw.service.SeekerService;
 import com.lx.odw.vo.JobVO;
+import com.lx.odw.vo.OfferVO;
 import com.lx.odw.vo.ProjectVO;
 
 @Controller
@@ -18,6 +22,8 @@ public class OfferController {
 
 	@Autowired
 	OfferService service;
+	OfferDAO offerDAO;
+	
 	
 	@RequestMapping(value="getOffList.do", method=RequestMethod.POST)
 	public @ResponseBody List<ProjectVO> getOffList(){
@@ -29,4 +35,25 @@ public class OfferController {
 		return service.requestOffJobListByProjectNumber(projectVO);
 		
 	}
+	
+	//web 일감 목록 
+	@RequestMapping(value="projectList.do")
+	public String projectList() {
+		System.out.println("projectList.do를 통해 웹페이지가 요청됨");
+		return "projectList";
+	}
+	
+	//web 사용자가 등록한 일감 목록
+	@RequestMapping(value="offerProjectList.do", method=RequestMethod.POST)
+	public String searchEmpListByDeptId(ProjectVO projectVO, HttpServletRequest request) {
+		//로그인에서 받아오는 부분
+		HttpSession session = request.getSession();//상태정보 로그인에서 한 
+		OfferVO offerVO = (OfferVO) session.getAttribute("loginInfo");
+		List<ProjectVO> list = offerDAO.projectList(projectVO);
+				
+		request.setAttribute("projectList", list);
+		
+		return "projectList";
+	}
+		
 }
