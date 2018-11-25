f46216436014b5f68bac4102384d226ce7fb5c5a
JeaWoonLee <33853326+JeaWoonLee@users.noreply.github.com>
2018-11-25 15:55:01 +0900

    사업자 서명정보 업로드 오류 수정

--- OneDayWork/src/main/java/com/lx/odw/service/OfferService.java
+++ OneDayWork/src/main/java/com/lx/odw/service/OfferService.java
@@ -80,7 +80,7 @@ public interface OfferService {
 
 	int updateOffer(String offerVO, HttpServletRequest request);
 
-	int updateOfferSign(MultipartFile OfferSign, String offerVO, HttpServletRequest request);
+	int updateOfferSign(MultipartFile OfferSign, String offer, HttpServletRequest request);
 
 	OfferVO requestOfferDetail(OfferVO offerVO);
 
